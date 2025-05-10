196a160ecca4261f058c6665545c5dfa97d56239
LuisOliveros11 <lparada_19@alu.uabcs.mx>
2025-05-09 21:05:45 -0700

    Implementacion enemigos nivel 1
    Se programó la aparición de los enemigos del nivel 1 junto con su descenso lento (Todavia no estan programadas las colisiones)

--- sketch.js
+++ sketch.js
@@ -4,38 +4,53 @@ let jugador = {
   vidas: 3,
   alto: 100,
   ancho: 70,
-  x: 350,
-  y: 670
+  x: 0,
+  y: 0
 };
 let listaMisiles = [];
+let listaEnemigos = [];
+let nivel = 1;
+let nivelTerminado = true;
 
 function preload() {
-  jugador.imagen = loadImage('https://imgs.search.brave.com/nkQ9s1cgC2AcJ1jZmtlDsxAZRREgspBMp-KWHPc2oRs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ZjUzOTQ2ODA2MGYy/ZTAwMDQ4NTgwN2Yu/cG5n');
+  jugador.imagen = loadImage('./img/nave.png');
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
+  jugador.x = windowWidth / 2 - jugador.ancho / 2;
+  jugador.y = windowHeight - 150;
 }
 
 function draw() {
-  background(220);
-  
+  background("black");
+
+  if(nivelTerminado){
+    nivelJuego();
+  }
+
   //DIBUJAR JUGADOR
-  image(jugador.imagen, jugador.x, jugador.y, jugador.ancho,jugador.alto);
-  
+  image(jugador.imagen, jugador.x, jugador.y, jugador.ancho, jugador.alto);
+
+  //DIBUJAR ENEMIGOS
+  for (let i = listaEnemigos.length - 1; i >= 0; i--) {
+    image(listaEnemigos[i].imagen, listaEnemigos[i].x, listaEnemigos[i].y, listaEnemigos[i].ancho, listaEnemigos[i].alto);
+    listaEnemigos[i].y += 0.5;
+  }
+
   //DIBUJAR MISILES
   eliminarMisil();
-  for (let i = listaMisiles.length - 1; i >= 0; i--){ 
+  for (let i = listaMisiles.length - 1; i >= 0; i--) {
     fill(listaMisiles[i].colorFondo)
     rect(listaMisiles[i].x, listaMisiles[i].y, listaMisiles[i].ancho, listaMisiles[i].alto);
-    listaMisiles[i].y-=5;
+    listaMisiles[i].y -= 5;
   }
-  
-    //CONTROLA MOVIMIENTO DEL JUGADOR 
-  if(keyIsDown(LEFT_ARROW)  && jugador.x > 0) {
+
+  //CONTROLA MOVIMIENTO DEL JUGADOR 
+  if (keyIsDown(LEFT_ARROW) && jugador.x > 0) {
     jugador.x -= 9;
   }
-  if(keyIsDown(RIGHT_ARROW)  && (jugador.x + jugador.ancho) < 800) {
+  if (keyIsDown(RIGHT_ARROW) && (jugador.x + jugador.ancho) < windowWidth) {
     jugador.x += 9;
   }
 }
@@ -47,8 +62,8 @@ function keyPressed() {
   }
 }
 
-function crearMisil(){
-    let misil = {
+function crearMisil() {
+  let misil = {
     alto: 60,
     ancho: 5,
     colorFondo: "red",
@@ -58,10 +73,31 @@ function crearMisil(){
   listaMisiles.push(misil);
 }
 
-function eliminarMisil(){
-   for (let i = listaMisiles.length - 1; i >= 0; i--){ 
-     if(listaMisiles[i].y < -60){
-       listaMisiles.splice(i, 1);
-     }
-   }
+function eliminarMisil() {
+  for (let i = listaMisiles.length - 1; i >= 0; i--) {
+    if (listaMisiles[i].y < -60) {
+      listaMisiles.splice(i, 1);
+    }
+  }
+}
+
+function nivelJuego() {
+  switch (nivel) {
+    case 1:
+      for (let i = 50; i <= 300; i += 100) {
+        for (let j = 700; j <= 1090; j += 130) {
+          let enemigo = {
+            imagen: loadImage('./img/nave_Enemiga.png'),
+            vidas: 1,
+            alto: 80,
+            ancho: 70,
+            x: j,
+            y: i
+          };
+          listaEnemigos.push(enemigo);
+        }
+      }
+      nivelTerminado = false;
+      break;
+  }
 }
